c4789e0b3c33a1ded7450d68107f00dd84c95628
SAJINASAJINA <shaleeshafi924@gmail.com>
2024-11-14 22:26:23 +0530

    first commit

---
+++ React project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Componets/Header';
+
+const App = () => {
+  return (
+    <div>
+      <h1>APP COMP</h1>
+      <content />
+      <Header />
+      
+    </div>
+  );
+};
+
+export default App;
